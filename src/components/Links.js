28188e1c0f50f97553506e0bf6f4a0c6a0cbc4fb
Ismail1802 <ismayileliyev96@icloud.com>
2022-12-30 21:39:00 +0200

    first commit

---
+++ src/components/Links.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import classes from "./Links.module.css";
+
+const Links = ({ value, short, clicked, setClicked }) => {
+  const copied = () => {
+    setClicked("Copied!");
+    navigator.clipboard.writeText(short);
+  };
+
+  return (
+    <article className={classes.links}>
+      <p className={classes["p-one"]}>{value}</p>
+      <hr />
+      <p className={classes["p-two"]}>{short}</p>
+      <button
+        onClick={copied}
+        className={classes.copy}
+        style={{
+          backgroundColor: clicked === "Copied!" ? "hsl(257, 27%, 26%)" : "",
+        }}
+      >
+        {clicked}
+      </button>
+    </article>
+  );
+};
+
+export default Links;
